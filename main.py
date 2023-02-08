9545dce91cf7b0c4faa2fa2f600989068d53a6b7
LagrangeH <maxsergeev39@gmail.com>
2023-02-08 04:00:08 +0300

    Connect OpenAI API

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 import sys
 
+import openai
 from loguru import logger as log
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import filters, ApplicationBuilder, ContextTypes, MessageHandler
 
 from config import Config
 
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+def openai_request(prompt: str) -> str:
+    return openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1024,
+        temperature=0.2,    # TODO: add user control and random option
+    )["choices"][0]["text"].strip()
 
 
-def main(config: Config):
+async def message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_html(
+        f"<b>{update.message.text}</b> {openai_request(prompt=update.message.text)}"
+    )
+
+
+@log.catch()
+def main(config: Config) -> None:
+    openai.api_key = config.OPENAI_API_KEY
 
     app = ApplicationBuilder().token(config.TELEGRAM_BOT_TOKEN).build()
 
-    app.add_handler(CommandHandler("hello", hello))
+    app.add_handler(MessageHandler(filters.TEXT, message))
 
     app.run_polling()
 
