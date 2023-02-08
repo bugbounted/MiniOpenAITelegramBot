9afccc9937216524b35f2ce651a062eb33af413d
LagrangeH <maxsergeev39@gmail.com>
2023-02-08 03:18:35 +0300

    Fix bug

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ env.read_env()
 @dataclass
 class Config:
     TELEGRAM_BOT_TOKEN = env.str("TELEGRAM_BOT_TOKEN")
-    TELEGRAM_USERS = env.list("TELEGRAM_USERS", validate=lambda n: all(user.decimal() for user in n))
+    TELEGRAM_USERS = env.list("TELEGRAM_USERS", validate=lambda n: all(user.isdecimal() for user in n))
     OPENAI_API_KEY = env.list("OPENAI_API_KEY")
