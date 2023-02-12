b79b71416bf98dd219137c925b115ce265bcff9a
Mohamad Samardeh Haghighi Fard <pmbeman@pm.me>
2023-02-12 12:13:20 +0330

    Update openai_api.py

--- src/openai_api.py
+++ src/openai_api.py
@@ -5,7 +5,7 @@ def openai_request(prompt: str) -> str:
     return openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
-        max_tokens=1024,
+        max_tokens=3800,
         temperature=0,    # TODO: add user control and random option
         top_p=1,
         frequency_penalty=0.5,
